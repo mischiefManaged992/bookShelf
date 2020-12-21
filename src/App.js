4807e6fcfaa985be10c46cf3b2af5f7584b83304
mischiefManaged992 <anusha.ravi92@tamu.edu>
2020-12-21 17:00:44 -0500

    added logo component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Logo from './components/logo/logo'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Logo />
       </div>
     );
   }
---
+++ src/components/logo/logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./logo.css";
+
+const logo = () => {
+    return(
+        <div className={classes.logo}>
+            <h1>testing</h1>
+        </div>
+    );
+}
+
+export default logo;
