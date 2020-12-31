1ca3e90ff43f35aab66737881c0bcf6b402d7e6d
mischiefManaged992 <anusha.ravi92@tamu.edu>
2020-12-31 11:00:01 -0500

    Added link component for header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Logo from './UI/logo/logo';
 import WhiteBtn from './UI/button/white-btn/white-btn';
+import Link from './UI/link/link';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+
 
       </div>
     );
---
+++ src/UI/link/link.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./link.css";
+
+const link = () => {
+    return(
+        <a href="#" className={classes.link}>Link</a> 
+    );
+}
+
+export default link;
